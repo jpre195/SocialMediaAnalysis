bb863d4ba214bd77a4167ec441358e48b6f761e8
jpre195 <jdpreston7@gmail.com>
2020-11-21 21:35:11 -0500

    Added a txt file with the keys.

---
+++ sentiment_analysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 21:33:26 2020
+
+@author: kaitr
+"""
+
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+
+
