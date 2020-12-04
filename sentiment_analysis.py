341ad31190775c8e5bbc3e76f2aef548e4ae59d4
jpre195 <jdpreston7@gmail.com>
2020-12-04 06:18:57 -0500

    Updated to make wordcloud for users hashtags and user mentions

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -34,13 +34,15 @@ def get_tweet_sentiment(tweet):
         '''
         # create TextBlob object of passed tweet text 
         analysis = TextBlob(clean_tweet(tweet))
-        # set sentiment 
-        if analysis.sentiment.polarity > 0: 
-            return 'positive'
-        elif analysis.sentiment.polarity == 0: 
-            return 'neutral'
-        else: 
-            return 'negative'
+#        # set sentiment 
+#        if analysis.sentiment.polarity > 0.2: 
+#            return 'positive'
+#        elif analysis.sentiment.polarity < 0.2 and analysis.sentiment.polarity > -0.2: 
+#            return 'neutral'
+#        else: 
+#            return 'negative'
+        
+        return analysis.sentiment.polarity
 
 def get_tweets(api, query, count = 100):
 
@@ -78,55 +80,203 @@ auth = OAuthHandler(api_key, api_secret_key)
 
 api = tweepy.API(auth)
 
-q1 = "pandemic OR COVID OR coronavirus"
+#q1 = "pandemic OR COVID OR coronavirus"
+##q1 = "lang:en"
+#
+#print('Getting tweets... ')
+#tweets = get_tweets(api, q1, count = 10000)
+#
+#n = 50
+#
+#for i in range(n):
+#    
+#    print(i + 1, 'out of', n, sep = ' ')
+#    
+#    time.sleep(30)
+#    more_tweets = get_tweets(api, q1, count = 10000)
+#
+#    for curr_tweet in more_tweets:
+#        
+#        tweets.append(curr_tweet)
+#    
+#    
+#    
+##positives = 0
+##negatives = 0
+#text = ''
+#
+#for tweet in tweets:
+#    
+##    if tweet['sentiment'] == 'positive':
+##        positives += 1
+##        
+##    if tweet['sentiment'] == 'negative':
+##        negatives += 1
+#        
+#    text = text + tweet['text']
+#
+##neutrals = len(tweets) - positives - negatives
+#
+##print('Positives: ', round((positives / len(tweets)) * 100, 2), '%', sep = '')
+##print('Neutrals: ', round((neutrals / len(tweets)) * 100, 2), '%', sep = '')
+##print('Negatives: ', round((negatives / len(tweets)) * 100, 2), '%', sep = '')
+#
+#stop_words = ['https', 'RT', 'http', 'will', 'know', 're', 'let'] + list(STOPWORDS)
+#
+#wordcloud = WordCloud(stopwords = stop_words, background_color = 'white').generate(text)
+#
+#plt.imshow(wordcloud, interpolation = 'bilinear')
+#plt.axis('off')
+#plt.show()
 
-print('Getting tweets... ')
-tweets = get_tweets(api, q1, count = 10000)
 
-n = 100
+#%%
 
-for i in range(n):
+#user = 'shoes2426'
+#user = 'RussPhillips11'
+user = 'GleasonZane'
+
+
+def user_word_cloud(user):
+    stop_words = ['one', 'time', 'https', 'RT', 'http', 'will', 'know', 're', 'let'] + list(STOPWORDS)
     
-    print(i + 1, 'out of', n, sep = ' ')
+    tweets = []
     
-    time.sleep(5)
-    more_tweets = get_tweets(api, q1, count = 10000)
-
-    for curr_tweet in more_tweets:
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name = user).items():
         
-        tweets.append(curr_tweet)
-    
-    
-
-positives = 0
-negatives = 0
-text = ''
-
-for tweet in tweets:
-    
-    if tweet['sentiment'] == 'positive':
-        positives += 1
+        parsed_tweet = {}
         
-    if tweet['sentiment'] == 'negative':
-        negatives += 1
+        parsed_tweet['text'] = clean_tweet(tweet.text)
+        parsed_tweet['sentiment'] = get_tweet_sentiment(tweet.text)
         
-    text = tweet['text']
+        tweets.append(parsed_tweet)
+        
+    text = ''
+    
+    for tweet in tweets:
+    
+        text = text + ' ' + tweet['text']
+    
+    
+    wordcloud = WordCloud(stopwords = stop_words, background_color = 'white').generate(text)
+    
+    plt.imshow(wordcloud, interpolation = 'bilinear')
+    plt.axis('off')
+    #plt.show()
+    plt.savefig(user + '.png', dpi = 2000)
+    
+    return tweets
 
-neutrals = len(tweets) - positives - negatives
+users = ['shoes2426', 'RussPhillips11', 'GleasonZane']
 
-print('Positives: ', round((positives / len(tweets)) * 100, 2), '%', sep = '')
-print('Neutrals: ', round((neutrals / len(tweets)) * 100, 2), '%', sep = '')
-print('Negatives: ', round((negatives / len(tweets)) * 100, 2), '%', sep = '')
+for user in users:
+    
+    user_word_cloud(user)
 
-wordcloud = WordCloud(background_color = 'white').generate(text)
+#%%
 
-plt.imshow(wordcloud, interpolation = 'bilinear')
-plt.axis('off')
-plt.show()
+def user_hashtag_word_cloud(username):
+    stop_words = ['one', 'time', 'https', 'RT', 'http', 'will', 'know', 're', 'let'] + list(STOPWORDS)
+    
+    tweets = []
+    
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items():
+        
+        parsed_tweet = {}
+        
+        hashtags = tweet.entities.get('hashtags')
+        
+        if len(hashtags) > 0:
+            
+            for i in range(len(hashtags)):
+                
+#                parsed_tweet['text'] = clean_tweet(hashtags[i]['text'])
+                parsed_tweet['text'] = hashtags[i]['text']
+                
+                parsed_tweet['sentiment'] = get_tweet_sentiment(hashtags[i]['text'])
+                
+                tweets.append(parsed_tweet)
+        
+#        parsed_tweet['text'] = clean_tweet(tweet.entities.get('hashtags')['text'])
+#        parsed_tweet['sentiment'] = get_tweet_sentiment(tweet.text)
+        
+#        tweets.append(parsed_tweet)
+        
+    text = ''
+    
+    for tweet in tweets:
+    
+        text = text + ' ' + tweet['text']
+    
+    
+    wordcloud = WordCloud(collocations = False, stopwords = stop_words, background_color = 'white').generate(text)
+    
+    plt.imshow(wordcloud, interpolation = 'bilinear')
+    plt.axis('off')
+    #plt.show()
+    plt.savefig(username + '_hashtags' + '.png', dpi = 2000)
+    
+    return tweets
+    
+    
+##user = 'shoes2426'
+#user = 'RussPhillips11'
+##user = 'GleasonZane'
+
+users = ['shoes2426', 'RussPhillips11', 'GleasonZane']
+
+test1 = user_hashtag_word_cloud(user)
+
+for user in users:
+    
+    user_hashtag_word_cloud(user)
 
 
+#%%
 
+def user_mentions_word_cloud(username):
+    stop_words = ['one', 'time', 'https', 'RT', 'http', 'will', 'know', 're', 'let'] + list(STOPWORDS)
+    
+    tweets = []
+    
+    for tweet in tweepy.Cursor(api.user_timeline, screen_name = username).items():
+        
+        parsed_tweet = {}
+        
+        user_mentions = tweet.entities['user_mentions']
+        
+        if len(user_mentions) > 0:
+            
+            for i in range(len(user_mentions)):
+                
+                parsed_tweet['text'] = user_mentions[i]['screen_name']
+                
+                parsed_tweet['sentiment'] = 0
+                
+                tweets.append(parsed_tweet)
+        
+        
+    text = ''
+    
+    for tweet in tweets:
+    
+        text = text + ' ' + tweet['text']
+    
+    
+    wordcloud = WordCloud(collocations = False, background_color = 'white').generate(text)
+    
+    plt.imshow(wordcloud, interpolation = 'bilinear')
+    plt.axis('off')
+    #plt.show()
+    plt.savefig(username + '_user_mentions' + '.png', dpi = 2000)
+    
+    return tweets
 
+users = ['shoes2426', 'RussPhillips11', 'GleasonZane']
+
+for user in users:
+    
+    user_mentions_word_cloud(user)
 
 
 
